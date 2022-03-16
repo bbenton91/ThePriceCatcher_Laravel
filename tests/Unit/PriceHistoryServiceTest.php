8a9e5e49c04fdfdce5a3ae5f7adba8592af3f685
bbenton91 <bbenton91.dev@gmail.com>
2022-03-16 15:57:54 -0400

    Test for the PirceHistoryService

--- tests/Unit/PriceHistoryServiceTest.php
+++ tests/Unit/PriceHistoryServiceTest.php
@@ -210,4 +210,76 @@ class PriceHistoryServiceTest extends TestCase
 
         DB::table('price_histories')->truncate();
     }
+
+    public function test_price_exists()
+    {
+        DB::table('price_histories')->truncate();
+
+        // Create our fake product
+        $apiProduct = (object)[
+            'sku' => 1,
+            'regularPrice' => 29.99,
+            'salePrice' => 28.99,
+        ];
+
+        // Create a model and save to the database
+        PriceHistory::factory()->create([
+            'product_sku' => 1,
+            'regular_price' => 2799,
+            'sale_price' => 2699,
+            'start_date' => now()->toDateTimeString(),
+        ]);
+
+        $this->assertDatabaseCount('price_histories', 1);
+
+        // Retrieve the model we just saved
+        $priceHistoryModel = PriceHistory::where('product_sku', 1)->orderBy('id', 'DESC')->first();
+
+        // Compare and get what needs to be resaved
+        $result = PriceHistoryService::CompareAPIResultsWithPriceHistory(collect([$apiProduct]), collect([$priceHistoryModel]), now()->addMinutes(1)->toDateTimeString());
+
+        // DB::transaction (function () use ($result) {
+            $result->each(function ($item) {
+                $item->save();
+            });
+        // });
+
+        $this->assertDatabaseCount('price_histories', 2);
+
+        $priceHistoryModel = PriceHistory::where('product_sku', 1)->orderBy('id', 'DESC')->first();
+
+        // error_log(print_r($priceHistoryModel));
+
+        $this->assertTrue($priceHistoryModel['sale_price'] == 2899);
+        $this->assertTrue($priceHistoryModel['regular_price'] == 2999);
+
+        /**
+         * Here we make a secondary product with the same prices and test if it will be added to the database.
+         *
+         */
+
+        $secondApiProduct = (object)[
+            'sku' => 1,
+            'regularPrice' => 29.99,
+            'salePrice' => 28.99,
+        ];
+
+        $priceHistoryModel = PriceHistory::where('product_sku', 1)->orderBy('id', 'DESC')->first();
+        error_log(print_r($priceHistoryModel));
+
+        $result = PriceHistoryService::CompareAPIResultsWithPriceHistory(collect([$secondApiProduct]), collect([$priceHistoryModel]), now()->addMinutes(2)->toDateTimeString());
+
+        $result->each(function ($item) {
+            $item->save();
+        });
+
+        // Make sure we still only have 2 entries. The third shouldn't have been added
+        $this->assertDatabaseCount('price_histories', 2);
+
+        $priceHistoryModel = PriceHistory::orderBy('id', 'DESC')->take(1)->first();
+
+        $this->assertTrue($priceHistoryModel['sale_price'] == 2899);
+        $this->assertTrue($priceHistoryModel['regular_price'] == 2999);
+
+    }
 }
