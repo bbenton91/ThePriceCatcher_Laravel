55ac2ead19cfb8ad3f4d2c478ebf4bdc1b7001a4
bbenton91 <bbenton91.dev@gmail.com>
2022-03-19 17:17:51 -0400

    Result page is working. Also cleaned up

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\BrowseController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ResultController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,18 +24,16 @@ Route::get('/browse', function() {
     return redirect("/browse/topSales/-1");
 });
 
-Route::get('/result/{id}', [ResultController::class, 'show']);
+Route::get('/search', function(Request $request) {
+    $searchQuery = $request->input('query');
+    return redirect()->route('search', ['searchQuery' => $searchQuery]);
+});
 
-// Route::get('/browse/topsales/{depId}', [BrowseController::class, 'showTopSales']);
-// Route::get('/browse/recentlychanged/{depId}', [BrowseController::class, 'showRecentlyChanged']);
-// Route::get('/browse/recentlyadded/{depId}', [BrowseController::class, 'showRecentlyAdded']);
+Route::get('/result/{id}', [ResultController::class, 'show']);
 
 Route::controller(BrowseController::class)->group(function(){
     Route::get('/browse/topSales/{depID}', 'showTopSales');
     Route::get('/browse/recentlyChanged/{depID}', 'showRecentlyChanged');
     Route::get('/browse/recentlyAdded/{depID}', 'showRecentlyAdded');
+    Route::get('/search/{searchQuery}', 'showSearch')->name('search');
 });
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
