b3285e1d059a223f10bd3c42803ed37c6dfe1278
bbenton91 <bbenton91.dev@gmail.com>
2022-03-23 17:31:36 -0400

    Added another controller

---
+++ app/Http/Controllers/SkuEmailController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Emails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SkuEmailController extends Controller
+{
+
+    
+}
